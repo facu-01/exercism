df306b087f909058da069fb9e17f166f36d74424
facu-01 <facundon.lucero@alumnos.frm.utn.edu.ar>
2023-01-09 01:07:37 -0300

    se actualiza solucion

--- fsharp/anagram/Anagram.fs
+++ fsharp/anagram/Anagram.fs
@@ -3,8 +3,9 @@ module Anagram
 open System
 
 let findAnagrams (sources: List<string>) (target: string) : List<string> =
-    let normalize string = string |> Seq.map Char.ToLower |> Seq.sort |> Seq.toList
+    let normalize string =
+        string |> Seq.map Char.ToLower |> Seq.sort |> Seq.toList
 
     sources
-    |> List.filter (fun w -> w.ToLower()   <> target.ToLower())
+    |> List.filter (fun w -> w.ToLower() <> target.ToLower())
     |> List.filter (fun w -> normalize w = normalize target)
