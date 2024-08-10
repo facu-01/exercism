d64a6152e0cc4d079b2e5376cf9f88e74f89df8b
Facundo Lucero <facundolucerofiorelli@gmail.com>
2024-08-10 23:52:49 +0000

    feat: se configura flake.nix para wsl

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -6,51 +6,28 @@
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , systems
-    , nix-vscode-extensions
-    , ...
-    } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = import nixpkgs {
-              system = system;
-              config.allowUnfree = true;
-            };
-            extensions = nix-vscode-extensions.extensions.${system};
-            codeWithExtensions =
-              let
-                inherit (pkgs) vscode-with-extensions;
-                someExtensions = with (extensions.forVSCodeVersion pkgs.vscode.version).vscode-marketplace; [
-                  haskell.haskell
-                  justusadam.language-haskell
-                  usernamehw.errorlens
-                ];
-              in
-              (vscode-with-extensions.override {
-                vscodeExtensions = someExtensions;
-              });
-          in
-          {
-            default = pkgs.mkShell {
+  outputs = { self, nixpkgs, systems, nix-vscode-extensions, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      devShells = forEachSystem (system:
+        let
+          pkgs = import nixpkgs {
+            system = system;
+            config.allowUnfree = true;
+          };
+        in {
+          default = pkgs.mkShell {
 
-              NIX_PATH = "nixpkgs=" + pkgs.path;
+            NIX_PATH = "nixpkgs=" + pkgs.path;
 
-              buildInputs = with pkgs;[
-                haskell-language-server
-                ghc
-                stack
-              ] ++ [ codeWithExtensions ];
+            shellHook = ''
+              WSLENV=$WSLENV:PATH
+            '';
 
-            };
-          });
+            buildInputs = with pkgs; [ haskell-language-server ghc stack ];
+
+          };
+        });
 
     };
 
