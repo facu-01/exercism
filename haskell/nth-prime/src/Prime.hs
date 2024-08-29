768ed67e55f58b3f05f841a4fbfd4e871d66998e
Facundo Lucero <facundolucerofiorelli@gmail.com>
2024-08-29 04:31:19 +0000

    finish nth-prime

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -4,17 +4,16 @@ import Control.Arrow
 import Data.Function
 import Data.List (elemIndex)
 
-primesToN :: Int -> [Int]
-primesToN 1 = [1, 0]
-primesToN 2 = [2, 1, 0]
-primesToN x
-  | notPrime = primes
-  | otherwise = x : primes
+nextPrime :: Int -> Int
+nextPrime 1 = 2
+nextPrime n
+  | notPrime = nextPrime (n + 1)
+  | otherwise = n + 1
   where
-    primes = primesToN (x - 1)
-    notPrime = any (\y -> x `mod` y == 0) primes
+    notPrime = any (\y -> (n + 1) `mod` y == 0) [2 .. n]
 
 nth :: Int -> Maybe Integer
-nth n = elemIndex n primes & fmap toInteger & fmap (+ 1)
-  where
-    primes = (primesToN >>> reverse) n
+nth n
+  | n == 0 = Nothing
+  | n == 1 = Just 2
+  | otherwise = nth (n - 1) & fmap (fromInteger >>> nextPrime >>> toInteger)
